e9bad4579d429ce724e513776b8d81f9d8ca3fcb
Cheruiyotd <korirkipnisden@gmail.com>
2020-07-24 02:25:05 +0300

    added floating context menu to hello world TextView

--- app/src/main/java/com/cheru/menutypes/MainActivity.java
+++ app/src/main/java/com/cheru/menutypes/MainActivity.java
@@ -3,9 +3,12 @@ package com.cheru.menutypes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.textTitle);
+        this.registerForContextMenu(tv);
     }
 
     @Override
@@ -39,5 +45,26 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.floating_context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.contextmenu1:
+                Toast.makeText(this, "Context item ! selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.contextmenu2:
+                Toast.makeText(this, " Context item 2 selected", Toast.LENGTH_LONG).show();
+                return true;
+                default:
+                    return super.onContextItemSelected(item);
+    }
     }
 }
